6bec3276f03342b14d44c8d3f4e13912cb958a5d
Kaspars <kaspars@konstruktors.com>
2013-06-25 13:40:03 +0200

    Add sample plugin container

---
+++ git-update-test.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Git Update Test
+Plugin URI: https://github.com/kasparsd/git-update-test
+GitHub URI: https://github.com/kasparsd/git-update-test
+Description: Test Git Update plugin
+Author: Kaspars Dambis
+Version: 0.1
+*/
